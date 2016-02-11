cb7dabb2d4ef3cf5aafd214b1f0dcb388c235704
Dina O'Connor <DinaOC@Dinas-MacBook-Pro.local>
2016-02-11 13:01:12 -0500

    added last levels

--- travelagency.js
+++ travelagency.js
@@ -16,7 +16,7 @@ console.log ("you have no idea what you are in to!");
                   }else if (answer === "b"){
                     answer =alert("You get only 1 points for trying to survive, but you are a mean person!");
                   }else if (answer === "c"){
-                    answer =alert("You get 2 point for trying to survive, but you are a quiter!");
+                    answer =alert("You get 1 point for trying to survive, but you are a quiter!");
                   }else {
                     answer = prompt("go back an start over");
                     location.reload();
@@ -39,10 +39,10 @@ console.log ("you have no idea what you are in to!");
                     answer =alert("Congrats! You get 3 points for trying so hard!");
 
                   }else if (answer ==="b"){
-                    answer =alert("You get only 2 points for quitting");
+                    answer =alert("You get only 1 points for quitting");
 
                   }else if (answer ==="c"){
-                    answer =alert("You get only 2 points for quitting");
+                    answer =alert("You get only 1 points for quitting");
                   }else {
                         location.reload();
                   }
@@ -53,7 +53,7 @@ console.log ("you have no idea what you are in to!");
 
 
           }else if (answer === "b") {
-            answer=prompt("are you going to a) live with monks, b) live on a rabbit island or c) on a cat island?");
+            answer=prompt("are you going to a) live with monks, b) live on a rabbit island or c)live on a cat island?");
                 if (answer === "a") {
                   answer =prompt("what do you choose: a)be silent for a month, b)pay extra money to go somewhere else or c)stay with monks, but do not follow the rules?");
                   if (answer ==="a"){
@@ -69,18 +69,46 @@ console.log ("you have no idea what you are in to!");
 
                 } else if (answer === 'b') {
                   answer =prompt("what do you choose: a)adopt a rabbit, b) cook one or c) pay extra to leave?");
+                  if (answer ==="a"){
+                    answer =alert("Congarts! You get 3 points for being so kind");
+                  }else if (answer ==="b") {
+                    answer =alert("You get only 1 point for surviving, but you are a mean person.");
+                  }else if (answer ==="c"){
+                    answer =alert("You get 0 points for being a quitter!");
+                  }else {
+                    answer = prompt("go back an start over");
+                    location.reload();
+                  }
 
                 }else if (answer === "c"){
-                  answer =prompt("what do you do: a) adopt a cat, b)move to the rabbit island or c)pay extra to leave?");
+                  answer =prompt("what do you do: a) adopt a cat, b)move to the rabbit island to eat rabbits c)pay extra to leave?");
+                  if (answer ==="a"){
+                    answer =alert("Congrats! You get 3 points for being so nice!");
+                  }else if (answer ==="b") {
+                    answer =prompt("You get only 1 point for trying to survive, but you are a mean person.");
+                  }else if (answer ==="c"){
+                    answer =alert("You get 0 points for being a quitter");
+                  }else {
+                    answer = prompt("go back an start over");
+                    location.reload();
+                  }
                 }else {
                   answer = prompt("go back an start over");
                   location.reload();
                 }
 
           } else if (answer ==="c") {
-            answer = prompt("Surprise! you are going to Federal States of Micronesia! what do you want to do: a) take the trip, b)pay extra to leave or c)try to escape for free?");
+            answer = prompt("Surprise! you are going to Federal States of Micronesia! what do you want to do: a) take the trip, b)pay extra not to go or c)try to escape from the Island?");
             if (answer ==="a") {
+                answer =prompt("Congrats! You get 3 points for trying something new!");
 
+            }else if ( answer ==="b"){
+              answer =prompt("You get 0 points for being a quitter");
+            }else if (answer ==="c"){
+              answer =prompt("You get 0 points for being a quitter")
+            }else {
+              answer = prompt("go back an start over");
+              location.reload();
             }
 
           }else {
@@ -90,15 +118,15 @@ console.log ("you have no idea what you are in to!");
           }
 
 
-    } else if (answer === 'not ready') { // Are you ready to choose your adventure: ready or not ready?
+    } else if (answer === 'not ready') {
     answer = prompt("get ready and say yes!")
     location.reload();
-    } else { // Are you ready to choose your adventure: ready or not ready?
+    } else {
     answer = prompt ("go back and start over");
     location.reload();
     }
-}else if (travelAgent ==='no'){ // "Can I help you?"
+}else if (travelAgent ==='no'){
   prompt("you need to say yes!");
-}else { // "Can I help you?"
+}else {
   prompt("just say yes!");
 }
