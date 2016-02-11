8fd8bb2320787bb7e2ebd90ccbf431c0d84401a0
Dina O'Connor <DinaOC@Dinas-MacBook-Pro.local>
2016-02-11 09:33:21 -0500

    added more options to the first part

--- travelagency.js
+++ travelagency.js
@@ -12,11 +12,11 @@ console.log ("you have no idea what you are in to!");
                 if ( answer === "a") {
                   answer = prompt("do you: a)make a tent, b)steal from the natives or c) move to the jungles?");
                   if (answer ==="a"){
-                    answer =prompt("Congrats! You get 3 points for being so smart!");
+                    answer =alert("Congrats! You get 3 points for being so smart!");
                   }else if (answer === "b"){
-                    answer =prompt("You get only 1 points for trying to survive, but you are a mean person!");
+                    answer =alert("You get only 1 points for trying to survive, but you are a mean person!");
                   }else if (answer === "c"){
-                    answer =prompt("You get 2 point for trying to survive, but you are a quiter!");
+                    answer =alert("You get 2 point for trying to survive, but you are a quiter!");
                   }else {
                     answer = prompt("go back an start over");
                     location.reload();
@@ -24,17 +24,28 @@ console.log ("you have no idea what you are in to!");
                 }else if ( answer === "b") {
                   answer =prompt("do you: a) hunt, b) steal from the natives or c) become vegetarian?");
                   if (answer === "a"){
-                    answer =prompt("Congrat! You get 3 point for hard work!");
+                    answer =alert("Congrat! You get 3 point for hard work!");
                   }else if (answer === "b"){
-                    answer =prompt("You get only 1 point for trying to survive, but you are a mean person!");
+                    answer =alert("You get only 1 point for trying to survive, but you are a mean person!");
                   }else if (answer ==="c"){
-                    answer =prompt("Congrats! You get 3 points for being so smart!");
+                    answer =alert("Congrats! You get 3 points for being so smart!");
                   }else {
                     answer = prompt("go back an start over");
                     location.reload();
                   }
                 }else if ( answer ==="c"){
                   answer =prompt("do you: a)learn the language, b) move to the beach c) move to the jungles?");
+                  if (answer ==="a"){
+                    answer =alert("Congrats! You get 3 points for trying so hard!");
+
+                  }else if (answer ==="b"){
+                    answer =alert("You get only 2 points for quitting");
+
+                  }else if (answer ==="c"){
+                    answer =alert("You get only 2 points for quitting");
+                  }else {
+                        location.reload();
+                  }
                 }else {
                   answer = prompt("go back an start over");
                   location.reload();
@@ -45,6 +56,16 @@ console.log ("you have no idea what you are in to!");
             answer=prompt("are you going to a) live with monks, b) live on a rabbit island or c) on a cat island?");
                 if (answer === "a") {
                   answer =prompt("what do you choose: a)be silent for a month, b)pay extra money to go somewhere else or c)stay with monks, but do not follow the rules?");
+                  if (answer ==="a"){
+                    answer =alert("Congrats! you get 3 points for trying something new!");
+                  }else if (answer ==="b"){
+                    answer =alert("You get 0 points for being a quitter!");
+                  }else if (answer ==="c"){
+                    answer =alert("You get only 1 point for staying, but you are a whimp!");
+                  }else {
+                    answer = prompt("go back an start over");
+                    location.reload();
+                  }
 
                 } else if (answer === 'b') {
                   answer =prompt("what do you choose: a)adopt a rabbit, b) cook one or c) pay extra to leave?");
@@ -56,7 +77,7 @@ console.log ("you have no idea what you are in to!");
                   location.reload();
                 }
 
-          } else if (answer ==="c") { 
+          } else if (answer ==="c") {
             answer = prompt("Surprise! you are going to Federal States of Micronesia! what do you want to do: a) take the trip, b)pay extra to leave or c)try to escape for free?");
             if (answer ==="a") {
 
