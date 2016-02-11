7474d43d76919230a24de40ce3655c1e08d57201
Dina O'Connor <DinaOC@Dinas-MacBook-Pro.local>
2016-02-11 14:21:19 -0500

    added objects and a function

--- travelagency.js
+++ travelagency.js
@@ -1,3 +1,18 @@
+var points = {
+smart: "Congrats! You get 2 points for being so smart!",
+kind:"Congarts! You get 2 points for being so kind",
+smthNew:"Congrats! you get 2 points for trying something new!",
+hardWork:"Congrat! You get 2 point for hard work!",
+mean: "You get only 1 points for trying to survive, but you are a mean person!",
+whimp:"You get only 1 point for staying, but you are a whimp!",
+quitter: "You get 0 points for being a quitter!",
+
+};
+
+
+
+
+
 console.log('This is an awesome "travel" agency');
 
 var travelAgent = prompt("Can I help you?");
@@ -12,43 +27,43 @@ console.log ("you have no idea what you are in to!");
                 if ( answer === "a") {
                   answer = prompt("do you: a)make a tent, b)steal from the natives or c) move to the jungles?");
                   if (answer ==="a"){
-                    answer =alert("Congrats! You get 3 points for being so smart!");
+                    alert(points.smart);
+
                   }else if (answer === "b"){
-                    answer =alert("You get only 1 points for trying to survive, but you are a mean person!");
+                    alert(points.mean);
                   }else if (answer === "c"){
-                    answer =alert("You get 1 point for trying to survive, but you are a quiter!");
+                    alert(points.quitter);
                   }else {
-                    answer = prompt("go back an start over");
-                    location.reload();
+                    startAgain();
                   }
+
                 }else if ( answer === "b") {
                   answer =prompt("do you: a) hunt, b) steal from the natives or c) become vegetarian?");
                   if (answer === "a"){
-                    answer =alert("Congrat! You get 3 point for hard work!");
+                    alert(points.hardWork);
                   }else if (answer === "b"){
-                    answer =alert("You get only 1 point for trying to survive, but you are a mean person!");
+                    alert(points.mean);
                   }else if (answer ==="c"){
-                    answer =alert("Congrats! You get 3 points for being so smart!");
+                    alert(points.smart);
                   }else {
-                    answer = prompt("go back an start over");
-                    location.reload();
+                startAgain();
                   }
+
                 }else if ( answer ==="c"){
                   answer =prompt("do you: a)learn the language, b) move to the beach c) move to the jungles?");
                   if (answer ==="a"){
-                    answer =alert("Congrats! You get 3 points for trying so hard!");
+                    alert(points.hardWork);
 
                   }else if (answer ==="b"){
-                    answer =alert("You get only 1 points for quitting");
+                    alert(points.quitter);
 
                   }else if (answer ==="c"){
-                    answer =alert("You get only 1 points for quitting");
+                    alert(points.quitter);
                   }else {
-                        location.reload();
+                  startAgain();
                   }
                 }else {
-                  answer = prompt("go back an start over");
-                  location.reload();
+                    startAgain();
                 }
 
 
@@ -57,63 +72,57 @@ console.log ("you have no idea what you are in to!");
                 if (answer === "a") {
                   answer =prompt("what do you choose: a)be silent for a month, b)pay extra money to go somewhere else or c)stay with monks, but do not follow the rules?");
                   if (answer ==="a"){
-                    answer =alert("Congrats! you get 3 points for trying something new!");
+                    alert(points.smthNew);
                   }else if (answer ==="b"){
-                    answer =alert("You get 0 points for being a quitter!");
+                    alert(points.quitter);
                   }else if (answer ==="c"){
-                    answer =alert("You get only 1 point for staying, but you are a whimp!");
+                    alert(points.whimp);
                   }else {
-                    answer = prompt("go back an start over");
-                    location.reload();
+                      startAgain();
                   }
 
                 } else if (answer === 'b') {
                   answer =prompt("what do you choose: a)adopt a rabbit, b) cook one or c) pay extra to leave?");
                   if (answer ==="a"){
-                    answer =alert("Congarts! You get 3 points for being so kind");
+                    alert(points.kind);
                   }else if (answer ==="b") {
-                    answer =alert("You get only 1 point for surviving, but you are a mean person.");
+                    alert(points.mean);
                   }else if (answer ==="c"){
-                    answer =alert("You get 0 points for being a quitter!");
+                    alert(points.quitter);
                   }else {
-                    answer = prompt("go back an start over");
-                    location.reload();
+                      startAgain();
                   }
 
                 }else if (answer === "c"){
                   answer =prompt("what do you do: a) adopt a cat, b)move to the rabbit island to eat rabbits c)pay extra to leave?");
                   if (answer ==="a"){
-                    answer =alert("Congrats! You get 3 points for being so nice!");
+                    alert(points.kind);
                   }else if (answer ==="b") {
-                    answer =prompt("You get only 1 point for trying to survive, but you are a mean person.");
+                    prompt(points.mean);
                   }else if (answer ==="c"){
-                    answer =alert("You get 0 points for being a quitter");
+                    alert(points.quitter);
                   }else {
-                    answer = prompt("go back an start over");
-                    location.reload();
+                        startAgain();
                   }
                 }else {
-                  answer = prompt("go back an start over");
-                  location.reload();
+                  startAgain();
                 }
 
           } else if (answer ==="c") {
             answer = prompt("Surprise! you are going to Federal States of Micronesia! what do you want to do: a) take the trip, b)pay extra not to go or c)try to escape from the Island?");
             if (answer ==="a") {
-                answer =prompt("Congrats! You get 3 points for trying something new!");
+                prompt(points.smtNew);
 
             }else if ( answer ==="b"){
-              answer =prompt("You get 0 points for being a quitter");
+              prompt(points.quitter);
             }else if (answer ==="c"){
-              answer =prompt("You get 0 points for being a quitter")
+              prompt(points.quitter)
             }else {
-              answer = prompt("go back an start over");
-              location.reload();
+              startAgain();
             }
 
           }else {
-            answer = prompt("go back an start over");
-            location.reload();
+              startAgain();
 
           }
 
@@ -122,7 +131,7 @@ console.log ("you have no idea what you are in to!");
     answer = prompt("get ready and say yes!")
     location.reload();
     } else {
-    answer = prompt ("go back and start over");
+    answer = prompt("go back and start over");
     location.reload();
     }
 }else if (travelAgent ==='no'){
@@ -130,3 +139,14 @@ console.log ("you have no idea what you are in to!");
 }else {
   prompt("just say yes!");
 }
+
+
+  function startAgain() {
+    while (answer !== "a,b,c") {
+      answer =confirm("press'OK' and start over");
+      location.reload();
+
+    }
+
+
+  }
